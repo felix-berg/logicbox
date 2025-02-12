f86f296c02a2d3a69592d668a6977692eb5b9ac7
Felix Berg <felixbpedersen@gmail.com>
2025-02-12 10:59:46 +0100

    remove validation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,9 +5,8 @@ object Main extends PLParser {
     val lexer = PLLexer()
     val parser = PLParser()
     val tokens = lexer.apply("(((p -> q) -> r) and (s -> not p) and (t) and (not s and t -> q)) -> r")
-    println(tokens)
     val formula = parser.apply(tokens)
-    println(formula)
-    println(s"Formula is ${if (PLValidator.validate(formula)) then "valid" else "invalid"}")
+
+
   }
 }
