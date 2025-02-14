7db2b526325058e0085e651e88188b1dda173c6c
Felix Berg <felixbpedersen@gmail.com>
2025-02-14 08:27:53 +0100

    switch back to case classes

--- src/main/scala/boxprover/proof/ProofDef.scala
+++ src/main/scala/boxprover/proof/ProofDef.scala
@@ -4,15 +4,8 @@ import boxprover.ViolationTrait
 
 sealed trait ProofStep[+F, +R <: Rule[F]]
 
-class ProofLine[+F, +R <: Rule[F]](val formula: F, val rule: R, val refs: List[ProofStep[F, R]]) extends ProofStep[F, R]
-object ProofLine {
-  def unapply[F, R <: Rule[F]](line: ProofLine[F, R]): Option[(F, R, List[ProofStep[F, R]])] =
-    Some(line.formula, line.rule, line.refs)
-}
+case class ProofLine[+F, +R <: Rule[F]](formula: F, rule: R, refs: List[ProofStep[F, R]]) extends ProofStep[F, R]
 
-class ProofBox[+F, +R <: Rule[F], +I](val info: I, val proof: Proof[F, R]) extends ProofStep[F, R]
-object ProofBox {
-  def unapply[F, R <: Rule[F], I](box: ProofBox[F, R, I]): Option[(I, Proof[F, R])] = Some (box.info, box.proof)
-}
+case class ProofBox[+F, +R <: Rule[F], +I](info: I, proof: Proof[F, R]) extends ProofStep[F, R]
 
 type Proof[+F, +R <: Rule[F]] = List[ProofStep[F, R]]
