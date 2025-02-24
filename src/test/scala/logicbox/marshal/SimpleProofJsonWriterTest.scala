c68ca0c224d9aacbdfdb89ddf13e1ba1daca235a
Felix Berg <felixbpedersen@gmail.com>
2025-02-24 09:53:39 +0100

    finished proof json writer

--- src/test/scala/logicbox/marshal/SimpleProofJsonWriterTest.scala
+++ src/test/scala/logicbox/marshal/SimpleProofJsonWriterTest.scala
@@ -17,7 +17,7 @@ class SimpleProofJsonWriterTest extends AnyFunSpec {
 
   private type W = JsonWriter[Proof[StubFormula, StubRule, ?, Id]]
   describe("SimpleProofJsonWriter::write") {
-    it("should write single node using delegates that simply return values") {
+    it("should write single line using dumb delegates") {
       val writer: W = SimpleProofJsonWriter(
         JsonWriter.func2Writer(_ => JsString("id")),
         JsonWriter.func2Writer(_ => JsString("formula")),
@@ -29,14 +29,92 @@ class SimpleProofJsonWriterTest extends AnyFunSpec {
         map = Map("step" -> StubLine())
       )
 
-      val exp = JsObject(
+      val stepJson = JsObject(
         "uuid" -> JsString("id"),
         "stepType" -> JsString("line"),
         "formula" -> JsString("formula"),
         "justification" -> JsString("justification"),
       )
 
-      writer.write(proof) shouldBe exp
+      writer.write(proof) shouldBe JsArray(stepJson)
+    }
+
+    val writer: W = SimpleProofJsonWriter(
+      JsonWriter.func2Writer { case id: Id => JsString(id) },
+      JsonWriter.func2Writer { case f: F => JsNumber(f.i) },
+      JsonWriter.func2Writer { case Justification(rule, refs) => JsObject(
+        "rule" -> JsString(rule.toString),
+        "refs" -> JsArray(refs.map(_.toString).map(JsString(_)).toList)
+      )}
+    )
+
+    it("should write single box using not-dumb delegates") {
+      val proof = StubProof(
+        rootSteps = Seq("box"),
+        map = Map(
+          "box" -> StubBox()
+        )
+      )
+
+      val stepJson = JsObject(
+        "stepType" -> JsString("box"),
+        "uuid" -> JsString("box"),
+        "proof" -> JsArray()
+      )
+
+      writer.write(proof) shouldBe JsArray(stepJson)
+    }
+
+    it("should write nested proof") {
+      val proof = StubProof(
+        rootSteps = Seq("l0", "box"), 
+        map = Map(
+          "l0" -> StubLine(StubFormula(0)),
+          "box" -> StubBox(
+            steps = Seq("l1", "l2")
+          ),
+          "l1" -> StubLine(StubFormula(1), rule = Bad()),
+          "l2" -> StubLine(formula = StubFormula(2), refs = Seq("l1"))
+        )
+      )
+
+      val json = JsArray(
+        JsObject(
+          "uuid" -> JsString("l0"),
+          "stepType" -> JsString("line"),
+          "formula" -> JsNumber(0),
+          "justification" -> JsObject(
+            "rule" -> JsString("Good()"),
+            "refs" -> JsArray()
+          )
+        ),
+        JsObject(
+          "stepType" -> JsString("box"),
+          "uuid" -> JsString("box"),
+          "proof" -> JsArray(
+            JsObject(
+              "uuid" -> JsString("l1"),
+              "stepType" -> JsString("line"),
+              "formula" -> JsNumber(1),
+              "justification" -> JsObject(
+                "rule" -> JsString("Bad()"),
+                "refs" -> JsArray()
+              )
+            ),
+            JsObject(
+              "uuid" -> JsString("l2"),
+              "stepType" -> JsString("line"),
+              "formula" -> JsNumber(2),
+              "justification" -> JsObject(
+                "rule" -> JsString("Good()"),
+                "refs" -> JsArray(JsString("l1"))
+              )
+            )
+          )
+        )
+      )
+
+      writer.write(proof) shouldBe json
     }
   }
 }
